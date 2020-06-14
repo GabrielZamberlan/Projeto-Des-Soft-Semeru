e9c3b0c830f0b6e65fac8d2db2811e1df437d217
pietrozneto <62669885+pietrozneto@users.noreply.github.com>
2020-06-14 16:16:58 -0300

    formas do jogador morrer e mudançca de fase

--- tela_jogo.py
+++ tela_jogo.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+from time import time
 from os import path
 
 img_dir = path.join(path.dirname(__file__), 'img')
@@ -19,10 +19,26 @@ SPEED_X = 10
 
 BLOCK = 0
 PLATF = 1
+LAVA = 2
 EMPTY = -1
 
-MAP = [
+MAP1 = [
     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, EMPTY, EMPTY, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, PLATF, EMPTY],
+    [BLOCK, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+    [BLOCK, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+    [BLOCK, BLOCK, BLOCK, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, LAVA, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+]
+MAP2 = [
+    [BLOCK, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
@@ -37,7 +53,6 @@ MAP = [
     [BLOCK, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
     [BLOCK, BLOCK, BLOCK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
 ]
-
 STILL = 0
 JUMPING = 1
 FALLING = 2
@@ -52,7 +67,7 @@ class Tile(pygame.sprite.Sprite):
         self.rect.y = TILE_SIZE * row
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, player_img, row, column, platforms, blocks):
+    def __init__(self, player_img, row, column, platforms, blocks, lava):
         pygame.sprite.Sprite.__init__(self)
         self.state = STILL
 
@@ -61,11 +76,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.platforms = platforms
         self.blocks = blocks
+        self.lava = lava
         self.rect.x = column * TILE_SIZE
         self.rect.bottom = row * TILE_SIZE
         self.speedx = 0
         self.speedy = 0
         self.highest_y = self.rect.bottom
+        self.fase1 = False
+        self.vivo = True
     def update(self):
         self.speedy += GRAVITY
         if self.speedy > 0:
@@ -91,6 +109,7 @@ class Player(pygame.sprite.Sprite):
                     self.highest_y = self.rect.bottom
                     self.speedy = 0
                     self.state = STILL
+                    self.fase1 = True
         self.rect.x += self.speedx
         if self.rect.left < 0:
             self.rect.left = 0
@@ -102,6 +121,11 @@ class Player(pygame.sprite.Sprite):
                 self.rect.right = collision.rect.left
             elif self.speedx < 0:
                 self.rect.left = collision.rect.right
+        collisions = pygame.sprite.spritecollide(self, self.lava, False)
+        for collision in collisions:
+            if self.speedy > 0:
+                self.rect.bottom = collision.rect.top
+                self.vivo = False
     def jump(self):
         if self.state == STILL:
             self.speedy -= JUMP_SIZE
@@ -110,21 +134,23 @@ def load_assets(img_dir):
     assets = {}
     assets[PLAYER_IMG] = pygame.image.load(path.join(img_dir, 'madeline.png')).convert_alpha()
     assets[BLOCK] = pygame.image.load(path.join(img_dir, 'grama.png')).convert()
-    assets[PLATF] = pygame.image.load(path.join(img_dir, 'grama.png')).convert()
+    assets[PLATF] = pygame.image.load(path.join(img_dir, 'estrela.jpg')).convert_alpha()
+    assets[LAVA] = pygame.image.load(path.join(img_dir,'lava.png')).convert()
     assets['BACKGROUND_IMG'] = pygame.image.load(path.join(img_dir, 'FUNDO MONTANHAS.png')).convert()
     return assets
 
 
-def game_screen(screen):
+def game_screen1(screen):
     clock = pygame.time.Clock()
     assets = load_assets(img_dir)
     all_sprites = pygame.sprite.Group()
     platforms = pygame.sprite.Group()
     blocks = pygame.sprite.Group()
-    player = Player(assets[PLAYER_IMG], 12, 2, platforms, blocks)
-    for row in range(len(MAP)):
-        for column in range(len(MAP[row])):
-            tile_type = MAP[row][column]
+    lava = pygame.sprite.Group()
+    player = Player(assets[PLAYER_IMG], 12, 2, platforms, blocks, lava)
+    for row in range(len(MAP1)):
+        for column in range(len(MAP1[row])):
+            tile_type = MAP1[row][column]
             if tile_type != EMPTY:
                 tile = Tile(assets[tile_type], row, column)
                 all_sprites.add(tile)
@@ -132,16 +158,16 @@ def game_screen(screen):
                     blocks.add(tile)
                 elif tile_type == PLATF:
                     platforms.add(tile)
+                elif tile_type == LAVA:
+                    lava.add(tile)
     all_sprites.add(player)
-    PLAYING = 0
-    DONE = 1
-    state = PLAYING
-    
-    while state != DONE:
+    game = True
+    while game :
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                state = DONE
+                game = False
+                state = 'quit'
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     player.speedx -= SPEED_X
@@ -154,12 +180,62 @@ def game_screen(screen):
                     player.speedx += SPEED_X
                 elif event.key == pygame.K_RIGHT:
                     player.speedx -= SPEED_X
-#            if Player.rect.x > 900
+            if player.vivo == False:
+                pygame.quit()
+                state = 'quit'
+            if player.fase1 == True:
+
+                state = 'game2'
         all_sprites.update()
         screen.blit(assets['BACKGROUND_IMG'], (0, 0))
         all_sprites.draw(screen)
         pygame.display.flip()
     return state
+def game_screen2(screen):
+    clock = pygame.time.Clock()
+    assets = load_assets(img_dir)
+    all_sprites = pygame.sprite.Group()
+    platforms = pygame.sprite.Group()
+    blocks = pygame.sprite.Group()
+    lava = pygame.sprite.Group()
+    player = Player(assets[PLAYER_IMG], 12, 2, platforms, blocks, lava)
+    for row in range(len(MAP2)):
+        for column in range(len(MAP2[row])):
+            tile_type = MAP2[row][column]
+            if tile_type != EMPTY:
+                tile = Tile(assets[tile_type], row, column)
+                all_sprites.add(tile)
+                if tile_type == BLOCK:
+                    blocks.add(tile)
+                elif tile_type == PLATF:
+                    platforms.add(tile)
+                elif tile_type == LAVA:
+                    lava.add(tile)
+    all_sprites.add(player)
+    game = True
+    while game:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = 'quit'
+                game = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx -= SPEED_X
+                elif event.key == pygame.K_RIGHT:
+                    player.speedx += SPEED_X
+                elif event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                    player.jump()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.speedx += SPEED_X
+                elif event.key == pygame.K_RIGHT:
+                    player.speedx -= SPEED_X
+        all_sprites.update()
+        screen.blit(assets['BACKGROUND_IMG'], (0, 0))
+        all_sprites.draw(screen)
+        pygame.display.flip()
+    return state    
 def menu(window):
     WIDTH = 1000
     HEIGHT = 520
@@ -168,17 +244,20 @@ def menu(window):
     image = pygame.image.load('img/imagem_menu.png').convert()
     font = pygame.font.SysFont(None, 100)
     text = font.render('Semeru', True, (0, 0, 0))
+    font = pygame.font.SysFont(None, 30)
+    text2 = font.render('Aperte qualquer tecla para continuar', True, (0,0,0))
     while rodando:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 state = 'quit'
                 rodando = False
             if event.type == pygame.KEYUP:
-                state = 'game'
+                state = 'game1'
                 rodando = False
 
         window.blit(image, (0, 0))
         window.blit(text, (150, 100))
+        window.blit(text2, (300, 300))
         pygame.display.update() 
 
     return state
@@ -189,8 +268,10 @@ state = 'menu'
 while state != 'quit':
     if state == 'menu':
         state = menu(screen)
-    elif state == 'game':
-        state = game_screen(screen)
+    elif state == 'game1':
+        state = game_screen1(screen)
+    elif state == 'game2':
+        state = game_screen2(screen)
     else:
         state = 'quit'
 pygame.quit()
